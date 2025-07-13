93b9f9df18574381fbbeb4382b923b263ce3519f
Intakhab2004 <ikbalam4648@gmail.com>
2025-07-13 12:44:52 +0530

    fixed bugs

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
